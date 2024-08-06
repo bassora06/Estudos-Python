d0394ee6c4ac8b5bddaa907795b6ec18a9a86844
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-06 14:34:46 -0300

    aulas 36 e 37
    criado a aula 36 sobre modulos, import e etc

---
+++ sessão 2/aula36.py
@@ -0,0 +1,14 @@
+# from sys import platform, exit
+# import sys as system # Apelido dado ao módulo sys
+# from sys import platform as pt # importando plataforma e dando o apelido de pt
+from sys import * # importando tudo má prática de programação
+
+# plataforma = platform
+# sys = 'alguma coisa'
+
+
+print(platform)
+# print(plataforma)
+
+# exit() # Termina o programa
+
