f3eaf90aa8308c08a43fc226e73661e71081f5c6
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-13 16:14:53 -0300

    aula recursão parte 2

--- sessão 2/aula50.py
+++ sessão 2/aula50.py
@@ -1,10 +1,20 @@
-def recursiva(inicio=0, fim= 10):
-    print(inicio, fim)
+# def recursiva(inicio=0, fim= 10):
+#     print(inicio, fim)
     
-    if inicio >= fim:
-        return fim
+#     if inicio >= fim:
+#         return fim
 
-    inicio += 1
-    return recursiva(inicio, fim)
+#     inicio += 1
+#     return recursiva(inicio, fim)
 
-print(recursiva())
+# print(recursiva())
+
+def fatorial(numero):
+
+    if numero <= 1:
+        return 1
+
+    return numero * fatorial(numero - 1)
+
+
+print(fatorial(5))
