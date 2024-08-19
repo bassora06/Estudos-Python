b394a30ca668d7fc346dc4a3853a7ff1d5289618
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-19 15:00:09 -0300

    melhorias no código

--- sessão 2/exec10.py
+++ sessão 2/exec10.py
@@ -1,3 +1,33 @@
+import os
+
+def listar():
+     print('Tarefas:')
+     if not listaDeTarefas:
+        print('Não há itens na lista de tarefas')
+        print()
+     else:
+        for i in listaDeTarefas:
+            print(f'{i}')
+        print()
+
+def desfazer():
+    if not listaDeTarefas:
+        print('Não há itens para desfazer')
+    else:
+        ultimoItem = len(listaDeTarefas) - 1
+        listaDesfeita.append(listaDeTarefas[ultimoItem])
+        listaDeTarefas.pop()
+    os.system('cls')
+
+def refazer():
+    if not listaDesfeita:
+        print('não há itens desfeitos')
+    else:
+        ultimoItem = len(listaDesfeita) - 1
+        listaDeTarefas.append(listaDesfeita[ultimoItem])
+        listaDesfeita.pop()
+    os.system('cls')
+
 listaDeTarefas = []
 listaDesfeita = []
 
@@ -7,25 +37,14 @@ while True:
     print()
 
 
-    if comando == 'listar' or comando =='Listar':
-        print('Tarefas:')
-        for i in listaDeTarefas:
-            print(f'{i}')
-    elif comando == 'desfazer' or comando == 'Desfazer':
-        if not listaDeTarefas:
-            print('Não há itens para desfazer')
-        else:
-            ultimoItem = len(listaDeTarefas) - 1
-            listaDesfeita.append(listaDeTarefas[ultimoItem])
-            listaDeTarefas.pop()
+    if comando == 'Listar' or comando =='listar':
+        listar()
+    elif comando == 'Desfazer' or comando == 'desfazer':
+        desfazer()
     elif comando == 'Refazer' or comando == 'refazer':
-            if not listaDesfeita:
-                 print('não há itens desfeitos')
-            else:
-                ultimoItem = len(listaDesfeita) - 1
-                listaDeTarefas.append(listaDesfeita[ultimoItem])
-                listaDesfeita.pop()
+        refazer()
     else:
         listaDeTarefas.append(comando)
+        os.system('cls')
 
 
