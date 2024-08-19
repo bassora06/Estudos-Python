83d22790cf81e5ce4581b7a088f8f7a75b08a854
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-19 13:33:32 -0300

    finalização de conteudo sobre arquivos

--- sessão 2/aula51.py
+++ sessão 2/aula51.py
@@ -1,4 +1,6 @@
-caminhoArquivo = 'C:\\Users\\pc\\Documents\\meuPc\\Estudos-Python-Modulo\\sessão 2\\'
+import os
+
+caminhoArquivo = 'C:\\Users\\pc\\Documents\\meuPc\\Estudos-Python-Modulo\sessão 2\\'
 caminhoArquivo += 'arquivo.txt'
 
 # arquivo = open(caminhoArquivo, 'r')
@@ -19,5 +21,10 @@ print('#' * 10, '\n')
 
 with open(caminhoArquivo, 'r')as arquivo:
     print(arquivo.read()) # le o arquivo
+    print('Aqui esta o arquivo')
+
+# os.remove(caminhoArquivo)
+print('Arquivo removido')
+os.rename(caminhoArquivo, 'bem vindo.txt') # renomeia o arquivo
     
 
