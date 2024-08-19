d815559fe2017ffc4618a86fe972a0cf02262215
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-19 12:55:39 -0300

    aula de arquivos
    aula de arquivos atualizada

--- sessão 2/aula51.py
+++ sessão 2/aula51.py
@@ -1,11 +1,23 @@
-caminhoArquivo = 'C:\\Users\\pc\\Documents\\meuPc\\Estudos-Python-Modulo\\sessão 2\\arquivo.txt'
-caminhoArquivo = 'arquivo.txt'
+caminhoArquivo = 'C:\\Users\\pc\\Documents\\meuPc\\Estudos-Python-Modulo\\sessão 2\\'
+caminhoArquivo += 'arquivo.txt'
 
 # arquivo = open(caminhoArquivo, 'r')
 
 # arquivo.close()
 
-with open(caminhoArquivo, 'w'):
-    print('Olá mundo')
-    print('arquivo fechado')
+with open(caminhoArquivo, 'w+')as arquivo:
+    arquivo.write('Seja bem vindo\n')
+    arquivo.write('Vagabundo\n') #escreve no arquivo
+    # arquivo.seek(0, 0)# mecher o cursos do arquivo
+    arquivo.writelines(
+        ('Linha 1\n', 'Linha 2\n', 'Linha 3\n', 'Linha 4\n')
+    )
+    arquivo.seek(0, 0)
+    print(arquivo.read()) # consigo ler o arquivo após modificar por conta do +
+
+print('#' * 10, '\n')
+
+with open(caminhoArquivo, 'r')as arquivo:
+    print(arquivo.read()) # le o arquivo
+    
 
