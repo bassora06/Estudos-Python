64926faf2b3219126f623538b724a6041ea3bdcc
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-19 12:12:33 -0300

    aula 51
    aprendendo sobre manipulação de arquivos, e with

--- sessão 2/aula51.py
+++ sessão 2/aula51.py
@@ -1,1 +1,11 @@
-print('oi')
+caminhoArquivo = 'C:\\Users\\pc\\Documents\\meuPc\\Estudos-Python-Modulo\\sessão 2\\arquivo.txt'
+caminhoArquivo = 'arquivo.txt'
+
+# arquivo = open(caminhoArquivo, 'r')
+
+# arquivo.close()
+
+with open(caminhoArquivo, 'w'):
+    print('Olá mundo')
+    print('arquivo fechado')
+
