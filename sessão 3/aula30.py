7480dd1ba5e888ebd91d52b5cd8e70baba37db05
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-21 15:51:21 -0300

    decoradores pt2

--- sessão 3/aula30.py
+++ sessão 3/aula30.py
@@ -14,12 +14,25 @@ class Time():
     def __init__(self, nome):
         self.nome = nome
 
+def myPlanet(metodo):
+    def interno(self, *args, **kwargs):
+          resultado = metodo(self, *args, **kwargs)
+
+          if 'Terra' in resultado:
+               return 'você está em casa'
+          return 'Você é um alien'
+    
+    return interno
 
 
 @adicionaRepr
 class Planeta():
     def __init__(self, nome):
         self.nome = nome
+
+    @myPlanet
+    def speakName(self):
+         return f'o planeta é {self.nome}'
 
 
 brasil = Time('Brasil')
@@ -33,4 +46,7 @@ print(brasil)
 print(argentina)
 print(terra)
 print(marte)
+
+print(terra.speakName())
+print(marte.speakName())
         
