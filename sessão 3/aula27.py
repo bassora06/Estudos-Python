022a7afd5894a60ca8e8c6701ea61889af4ac0c2
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-21 15:20:16 -0300

    context manager pt2

--- sessão 3/aula27.py
+++ sessão 3/aula27.py
@@ -13,12 +13,21 @@ class ContextManager():
     def __exit__(self, classException_, exception_, traceback_):
         print('Fechando arquivo')
         self._arquivo.close()
-        print('Exit')
+        
+        # raise classException_('Minha mensagem')
+    
+        # print(classException_)
+        # print(exception_)
+        # print(traceback_)
+
+        # return True
+
+        exception_.add_note('Minha nota')
 
 instancia = ContextManager('arquivo222.txt', 'w')
 
 with instancia as arquivo:
     arquivo.write('Bem vindo a aula de context manager\n')
-    arquivo.write('Aula 27\n')
+    arquivo.write('Aula 27\n', 123)
     arquivo.write('Aluno: Lucas')
     print('With', arquivo)
