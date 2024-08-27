2a19419a4f6d15ae8f9c566ff7514edf0bb1acd4
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-27 14:51:27 -0300

    exemplo de função documentada

--- sessão 3/aula34.py/exemploFuncao.py
+++ sessão 3/aula34.py/exemploFuncao.py
@@ -8,5 +8,24 @@ def soma(x: int | float, y: int | float) -> int | float:
 
     return x + y
 
+
+def multiplica(
+        x: int | float,
+        y: int | float,
+        z: int | float | None = None
+) -> int | float:
+    if z == None:
+        return x * y
+    return x * y * z
+
+"""
+Multiplica x e y
+caso z não seja none ele multiplica o z também
+"""
+
+
 soma = soma(3.6, 4)
 print(soma)
+
+multiplicar = multiplica(3, 4, 6)
+print(multiplicar)
