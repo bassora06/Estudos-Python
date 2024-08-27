7fffd7a506a9879689742009c7b955d2debc84c8
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-27 14:46:02 -0300

    docStrings

---
+++ sessão 3/aula34.py/exemploFuncao.py
@@ -0,0 +1,12 @@
+def soma(x: int | float, y: int | float) -> int | float:
+    """
+    Está função recebe 2 parametros
+    x: pode ser int ou float
+    y: pode ser int ou float
+    e o retorno por ser int ou float
+    """
+
+    return x + y
+
+soma = soma(3.6, 4)
+print(soma)
--- sessão 3/aula34.py/variasLinhas.py
+++ sessão 3/aula34.py/variasLinhas.py
@@ -1,6 +1,9 @@
-'''
+"""
 oi bom dia
 tudo bem?
 este é um exemplo de documentação
 com várias linhas
-'''
+
+A PEP8 recomenda fazer comentários com aspas duplas.
+maximo 80 caracteres por linha.
+"""
