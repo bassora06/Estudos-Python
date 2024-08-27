c1c578da3a9ae9f1686f5f5d459340f2d283bf7e
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-27 10:03:57 -0300

    metaclass e decorador

---
+++ sessão 3/aula32.py
@@ -0,0 +1,17 @@
+class Multiplicar():
+    def __init__(self, multiplicador):
+        self._multiplicador = multiplicador
+
+    def __call__(self, func):
+        def interna(*args, **kwargs):
+            resultado = func(*args, **kwargs)
+            return resultado * self._multiplicador
+        return interna
+
+@Multiplicar(10)
+def soma(x, y):
+    return x + y
+
+doisMaisDois = soma(2, 2)
+print(doisMaisDois)
+        
