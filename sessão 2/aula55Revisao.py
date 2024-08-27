a1eef6ab16655e02cc964abb2d9159c3d56d6ea1
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-27 09:44:14 -0300

    generator revisão

--- sessão 2/aula55Revisao.py
+++ sessão 2/aula55Revisao.py
@@ -1,5 +1,14 @@
+import sys
+
 iterable = ['oi', 'bom dia', 'tudo bem?']
 iterator = iter(iterable)
+
+lista = [n for n in range(10000000)]
+generator = (n for n in range(100000000))
+
+
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
 
 for i in iterable:
     print(next(iterator))
