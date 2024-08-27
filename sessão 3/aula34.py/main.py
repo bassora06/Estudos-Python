23252e7bf10068046f16df1f15e441a6fdd5c0a0
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-08-27 14:40:06 -0300

    arquivo de varias linhas

--- sessão 3/aula34.py/main.py
+++ sessão 3/aula34.py/main.py
@@ -1,8 +1,9 @@
 import uma_linha
 
-print()
-print(dir(uma_linha))
-print(uma_linha.__doc__)
-print(uma_linha.__file__)
-print(uma_linha.__name__)
-print()
+# print()
+# print(dir(uma_linha))
+# print(uma_linha.__doc__)
+# print(uma_linha.__file__)
+# print(uma_linha.__name__)
+# print()
+# help(uma_linha)
---
+++ sessão 3/aula34.py/variasLinhas.py
@@ -0,0 +1,6 @@
+'''
+oi bom dia
+tudo bem?
+este é um exemplo de documentação
+com várias linhas
+'''
